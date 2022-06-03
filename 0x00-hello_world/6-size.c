438010e05a90638ece9d56c91d3c3f3d10af48cf
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-02 20:58:31 -0700

    Printing values of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	char single_character;
-	int number;
-	long int a_long_number;
-	long long int a_long_long_number;
-	float a_float_number;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(single_character));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(number));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a_long_number));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(a_long_long_number));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(a_float_number));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 	return (0);
 }
