846d2445e930f9631dc745024db2e83ef4e8a124
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-04 04:18:57 -0700

    Print text to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,20 +9,10 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-09\n";
-	char strerr[] = "Error, no string detected\n";
 	int n;
-	int x;
 
 	n = strlen(str);
-	x = strlen(strerr);
-	if (n > 0)
-	{
-		fwrite(str, n, 1, stderr);
-	}
-	else
-	{
-		fwrite(strerr, x, 1, stderr);
-	}
+	fwrite(str, n, 1, stderr);
 
 	return (1);
 }
