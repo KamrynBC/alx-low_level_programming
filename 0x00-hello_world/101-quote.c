32e30a53f3e9164ee1f6bbcfda10e9b371adab8d
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-04 04:14:14 -0700

    Printiing a string of detemined size to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,20 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-09\n";
+	char strerr[] = "Error, no string detected\n";
 	int n;
+	int x;
 
 	n = strlen(str);
-	fwrite(str, n, 1, stderr);
+	x = strlen(strerr);
+	if (n > 0)
+	{
+		fwrite(str, n, 1, stderr);
+	}
+	else
+	{
+		fwrite(strerr, x, 1, stderr);
+	}
 
 	return (1);
 }
