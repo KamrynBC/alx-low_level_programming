0efc7656b8b9810a69456da6d3b2868acdf706b2
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-05 06:15:35 -0700

    Print every possible different combination 0f 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i < '7'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					i = i;
+					j = j;
+					k = k;
+				}
+				else if (i < j && j > k)
+				{
+					i = i;
+					j = j;
+					k = j + 1;
+				}
+				else if (i < j && j == k)
+				{
+					i = i;
+					j = j;
+					k = j + 1;
+				}
+				else if (i > j && j < k)
+				{
+					i = i;
+					j = i + 1;
+					k = j + 1;
+				}
+				else if (i > j && j > k)
+				{
+					i = i;
+					j = i + 1;
+					k = j + 1;
+				}
+				else if (i > j && j == k)
+				{
+					i = i;
+					j = i + 1;
+					k = j + 1;
+				}
+				else if (i == j && j < k)
+				{
+					i = i;
+					j = i + 1;
+					k = j + 1;
+				}
+				else if (i == j && j > k)
+				{
+					i = i;
+					j = i + 1;
+					k = j + 1;
+				}
+				else if (i == j && j == k)
+				{
+					i = i;
+					j = i + 1;
+					k = j + 1;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	fwrite("789", 3, 1, stdout);
+	putchar('\n');
+
+	return (0);
+}
