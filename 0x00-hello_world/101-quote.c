72f17483bdc6561aed45e1fde2611846f2b091e1
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-05 15:14:43 -0700

    Printing a string of text, followed by a new line character, to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-09\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int n;
 
 	n = strlen(str);
