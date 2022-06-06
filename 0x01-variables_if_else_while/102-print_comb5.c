8e42127f5b4b1b749fed3a341e188342709656a8
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-06 01:22:07 -0700

    Printing combo of two two digit number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (m = '0'; m <= '9'; m++)
+				{
+					i = i;
+					j = j;
+					k = k;
+					if (j > m)
+					{
+						m = j + 1;
+					}
+					else if (j == m)
+					{
+						m = j + 1;
+					}
+					else if (j < m)
+					{
+						m = m;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(m);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	fwrite("98 99", 5, 1, stdout);
+	putchar('\n');
+
+	return (0);
+}
