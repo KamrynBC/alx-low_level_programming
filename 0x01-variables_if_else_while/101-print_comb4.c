d3a5d12806e39d48418e5c2e08dd408af8adc575
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-06 00:37:54 -0700

    Printing all possible lowest 3 digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,64 +13,10 @@ int main(void)
 
 	for (i = '0'; i < '7'; i++)
 	{
-		for (j = '0'; j < '9'; j++)
+		for (j = i + 1; j <= '8'; j++)
 		{
-			for (k = '0'; k <= '9'; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
-				if (i < j && j < k)
-				{
-					i = i;
-					j = j;
-					k = k;
-				}
-				else if (i < j && j > k)
-				{
-					i = i;
-					j = j;
-					k = j + 1;
-				}
-				else if (i < j && j == k)
-				{
-					i = i;
-					j = j;
-					k = j + 1;
-				}
-				else if (i > j && j < k)
-				{
-					i = i;
-					j = i + 1;
-					k = j + 1;
-				}
-				else if (i > j && j > k)
-				{
-					i = i;
-					j = i + 1;
-					k = j + 1;
-				}
-				else if (i > j && j == k)
-				{
-					i = i;
-					j = i + 1;
-					k = j + 1;
-				}
-				else if (i == j && j < k)
-				{
-					i = i;
-					j = i + 1;
-					k = j + 1;
-				}
-				else if (i == j && j > k)
-				{
-					i = i;
-					j = i + 1;
-					k = j + 1;
-				}
-				else if (i == j && j == k)
-				{
-					i = i;
-					j = i + 1;
-					k = j + 1;
-				}
 				putchar(i);
 				putchar(j);
 				putchar(k);
