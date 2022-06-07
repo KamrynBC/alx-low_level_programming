c3b7f500f1b99497c3cd28c127c2908b16b6f875
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-07 12:56:13 -0700

    Checking if value given is a lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,19 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c - The integer to print
+ * _islower - Entry point
+ * @c: Integer argument to be checked
  *
+ * Return: 1 if the number is lower case alphabet. 0 if otherwise
  */
 int _islower(int c)
 {
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
