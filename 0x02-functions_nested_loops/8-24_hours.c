b9a40ba703e4bc635c686167fd173703e2f4f7ba
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-07 16:40:30 -0700

    Prints every minute of the day of Jack Bauer, from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,4 +7,28 @@
  */
 void jack_bauer(void)
 {
+	int i = '0', j, k, m;
+
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; (i <= '1' && j <= '9') || (i == '2' && j < '4'); j++)
+		{
+			for (k = '0'; (j <= '9' && k <= '5'); k++)
+			{
+				for (m = '0'; (k <= '5' && m <= '9'); m++)
+				{
+					i = i;
+					j = j;
+					k = k;
+					m = m;
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
