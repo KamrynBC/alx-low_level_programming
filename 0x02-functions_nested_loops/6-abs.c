029593b3d9b39bde7ec4f6017e817dbeb8bd9ea1
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-07 14:05:08 -0700

    Print absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,21 @@
 
 /**
  * _abs - returns an absolute value of the integer
- * @int - value that has test ran on it
+ * @i: value that has test ran on it
+ *
+ * Return: i if positive. Otherwise i/-1
  *
  */
-int _abs(int)
+int _abs(int i)
 {
+	if (i >= 0)
+	{
+		i = i;
+		return (i);
+	}
+	else
+	{
+		i = i / -1;
+		return (i);
+	}
 }
