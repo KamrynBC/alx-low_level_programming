7935b53028a128ace4a30bf94fa6f4ac06e2c7c2
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-07 17:30:04 -0700

    Printing a times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,23 @@
 
 /**
  * times_table - prints the 9 times tables
- * @void - no argument
+ * @void: no argument
  *
  */
 void times_table(void)
 {
+	char i, j, x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		x = i * j;
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(x);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
