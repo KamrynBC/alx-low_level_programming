fc4f79c23a9c8b77b9c8161c350f1cdb0f814a52
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-08 14:16:45 -0700

    Printing the times table of n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_times_table - entry point
+ * @n: integer value to be tested on
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+
+	int i, j, x, y;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				x = i * j;
+				_putchar(x);
+				_putchar(',');
+				_putchar(' ');
+			}
+			y = n * n;
+			_putchar(y);
+			_putchar('\n');
+		}
+	}
+}
