c0671238b5eb09089d0b9e481ec2fdf7a3e7777b
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-08 04:17:54 -0700

    Adding numbers that are multiples of 3 or 5 from 0 to 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, total;
+
+	total = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			total = total + i;
+		}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
