aedd969b9965b24309c472412492e81bc115aac0
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-09 11:45:23 -0700

    Prints all numbers from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,4 +7,18 @@
  */
 void print_most_numbers(void)
 {
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			i = i;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
 }
