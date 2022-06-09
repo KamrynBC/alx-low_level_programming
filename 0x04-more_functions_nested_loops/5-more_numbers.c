4aba73a4277e86a14d650814a003a731bac14c4e
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-09 11:08:27 -0700

    Print more number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,4 +7,24 @@
  */
 void more_numbers(void)
 {
+	int i, j, k, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			k = j;
+			if (j > 9)
+			{
+				while (k > 9)
+				{
+					k = k / 10;
+				}
+				_putchar(k);
+			}
+			m = j % 10;
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
 }
