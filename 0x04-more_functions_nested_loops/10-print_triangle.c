b7d4f0ea129c515571d303cccd7620be1573a8c1
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-09 18:04:42 -0700

    Prints a triangle followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,4 +8,40 @@
  */
 void print_triangle(int size)
 {
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = size; i > 0; i--)
+		{
+			j = size - (i - 1);
+			k = i - 1;
+			if (k >= 0)
+			{
+				while (k > 0)
+				{
+					_putchar(' ');
+					k--;
+				}
+				if (j > 0)
+				{	
+					while (j > 0)
+					{
+						_putchar('#');
+						j--;
+					}
+					_putchar('\n');
+				}
+				else if (j <= 0)
+				{
+					_putchar('#');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
