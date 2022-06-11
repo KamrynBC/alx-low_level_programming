4c8498df628f3b1aa88b595fbbe4d016e9dad5b7
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-11 06:24:31 -0700

    Print times table n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,21 +9,46 @@
 void print_times_table(int n)
 {
 
-	int i, j, x, y;
+	int i, j, x, y, m, k, z, a;
 
 	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		_putchar('0');
+		for (y = 1; y <= n; y++)
 		{
-			for (j = 0; j < n; j++)
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0');
+		}
+		_putchar('\n');
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
 				x = i * j;
-				_putchar(x);
+				m = x % 10;
+				k = x / 10;
+				z = k / 10;
+				a = k % 10;
 				_putchar(',');
 				_putchar(' ');
+				if (k >= 1 && k < 10)
+				{
+					_putchar(k + '0');
+					_putchar(m + '0');
+				}
+				else if (k <= 1)
+				{
+					_putchar(m + '0');
+				}
+				if (k >= 10)
+				{
+					_putchar(z + '0');
+					_putchar(a + '0');
+					_putchar(m + '0');
+				}
 			}
-			y = n * n;
-			_putchar(y);
 			_putchar('\n');
 		}
 	}
