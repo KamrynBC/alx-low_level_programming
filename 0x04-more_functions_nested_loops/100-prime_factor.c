13f25b43b670f81ffbc8aa44644378e1a8bfb71d
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-12 03:47:13 -0700

    Prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	long int i;
+
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		printf("%ld ", n);
+	}
+	printf("\n");
+
+	return (0);
+}
