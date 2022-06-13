e876dc45e007042b69663d160679dfe852d37ca1
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-13 11:23:06 -0700

    Print the second half of a given string and only the second half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -28,7 +28,7 @@ void puts_half(char *str)
 	}
 	else if (len % 2 != 0)
 	{
-		for (i = (len - 1) / 2; i <= len - 1; i = i + 1)
+		for (i = (len + 1) / 2; i <= len - 1; i = i + 1)
 		{
 			_putchar(str[i]);
 		}
