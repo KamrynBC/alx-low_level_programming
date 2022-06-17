29aefabcb4cb0ba609d4ef7b92f266bad314700d
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-17 14:35:54 -0700

    Print any number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n: integer to be tested on
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i, j, k, x, m;
+
+	if (n > 0 || n < 0)
+	{
+		for (i = 9; i >= 0; i--)
+		{
+			m = 1;
+			j = 1;
+			while (j <= i)
+			{
+				m = m * 10;
+				j++;
+			}
+			if (n > 0)
+			{
+				if (n >= m)
+				{
+					k = n / m;
+					x = k % 10;
+					_putchar(x + '0');
+				}
+			}
+			else if (n < 0)
+			{
+				_putchar('-');
+				n = n / -1;
+				if (n >= m)
+				{
+					k = n / m;
+					x = k % 10;
+					_putchar(x + '0');
+				}
+			}
+		}
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+}
+
