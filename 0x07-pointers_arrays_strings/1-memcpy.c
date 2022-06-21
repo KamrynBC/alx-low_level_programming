a3025be3c8c29c07b65381f02639ca554fc6b303
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-20 17:35:03 -0700

    test

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,5 +10,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i, j, k;
+
+	i = 0;
+	k = 50 + n;
+	for (j = 50; j < k; j++)
+	{
+		dest[j] = src[i];
+		i++;
+	}
+
 	return (dest);
 }
