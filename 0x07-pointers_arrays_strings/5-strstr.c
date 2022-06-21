8f70a5071767927e193968dfb5c841c317f964b7
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-20 20:26:19 -0700

    Locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,5 +9,31 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	return (needle);
+	char *point;
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j] && haystack[i] != *point)
+			{
+				point = &haystack[i];
+				break;
+			}
+			else
+			{
+				j++;
+			}
+		}
+		i++;
+	}
+	if (haystack[i] == '\0')
+	{
+		point = &haystack[i];
+	}
+
+	return (point);
 }
