079f645c9099a57cb56676d73021a324d057c83c
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-23 01:06:46 -0700

    Factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,5 +8,19 @@
  */
 int factorial(int n)
 {
-	return ();
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (n == 1 || n == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (n * factorial(n - 1));
+		}
+	}
 }
