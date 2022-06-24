ed733c850a3b8a4468c60e74c729477d618aca66
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-23 18:17:07 -0700

    Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,25 @@
  *
  * Return: integer 1 if prime. 0 if otherwise
  */
-int _is_prime_number(int n)
+int is_prime_number(int n)
 {
-	return ();
+	if (n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		if (n == 2)
+		{
+			return (1);
+		}
+		else if (n != 2 && n % 2 == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (0);
+		}
+	}
 }
