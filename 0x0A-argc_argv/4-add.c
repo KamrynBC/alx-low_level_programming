c9df77606f307cb28931e4517482e1cd8ce97e63
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-24 17:53:30 -0700

    Add poitive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: Number of arguements
+ * @argv: String arguement vector
+ *
+ * Return: Answer
+ */
+int main(int argc, char **argv)
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+			{
+				sum = sum + atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+
+				return (1);
+				break;
+			}
+		}
+		printf("%d\n", sum);
+
+		return (0);
+	}
+	else
+	{
+		i = 0;
+		printf("%d\n", i);
+
+		return (0);
+	}
+}
