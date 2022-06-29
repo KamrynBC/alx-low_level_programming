27e3f46ecbacfad651c1b74cb02d9bf1d658121a
Kamryn Chetty <kamrynchetty1203@gmail.com>
2022-06-29 15:22:02 -0700

    Push header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,5 @@
 int _putchar(char c);
 int main(void);
-int main(int argc, char **argv);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
